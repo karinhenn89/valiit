2d8cdccfacf46ff7c45ce86648059ca77cbcbcde
Karin <karinhenn89@gmail.com>
2024-11-04 14:36:13 +0200

    Hello world

--- src/DayOne.java
+++ src/DayOne.java
@@ -2,7 +2,22 @@
 public class DayOne {
     public static void main(String[] args) {
 
-        System.out.println("Karin");
+        // System.out.println("Karin");
+
+        // if conditions - print out "b on bigger than a"
+
+
+        int a = 1;
+        int b = 2;
+
+        if (b > a){
+            System.out.println("b on suurem kui a");
+        } else if (a > b){
+            System.out.println("b on väiksem kui a");
+        } else{
+            System.out.println("b võrdub a");
+        }
+
 
     }
 }
