07563ab9b7a27f41bc2b884c2a19d715b6a07f64
Karin <karinhenn89@gmail.com>
2024-11-04 15:13:16 +0200

    Hello world

--- src/DayOne.java
+++ src/DayOne.java
@@ -5,6 +5,9 @@ public class DayOne {
         // System.out.println("Karin");
 
         // if conditions - print out "b on bigger than a"
+        // If, If else, Else - b on suurem kui a , a on suurem kui b, a ja b on võrdsed
+
+        /*
 
 
         int a = 1;
@@ -17,7 +20,28 @@ public class DayOne {
         } else{
             System.out.println("b võrdub a");
         }
+                                */
 
+
+      //print out "b is bigger than a , andb a is positive"
+      //print out "a is bigger than b, and  b is positive" in else if block
+      //print out "a and b are either equal on both are negative" in else if block//print out "no specific conditions were met" in else block
+
+
+        int a = 1;
+        int b = 2;
+
+        if (b>a && a>=0) {
+            System.out.println("b is bigger than a, and a is positive");
+        } else if (a>b && b>=0) {
+            System.out.println("a is bigger than b, and b is positive");
+        } else if (a==b || (a<0 && b<0)) {
+            System.out.println("a and b are either equal or both are negative");
+        } else {
+            System.out.println("No specific conditions were met");
+        }
+
+        }
 
     }
-}
+
