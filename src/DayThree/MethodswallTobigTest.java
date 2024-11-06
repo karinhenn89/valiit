c52fe6da0f4ebf69de514393f97ae05ec95bc88f
Karin <karinhenn89@gmail.com>
2024-11-06 15:03:02 +0200

    Harjutused

--- src/DayThree/MethodswallTobigTest.java
+++ src/DayThree/MethodswallTobigTest.java
@@ -1,19 +1,36 @@
 package DayThree;
 
-import java.util.Scanner;
 
 public class MethodswallTobigTest {
-    // meetod kirjutab sisestatud nime suurte läbivate tähtedega
+
+    // loo uus meetod, mis arvutab ruudu pindala (private static void getArea). kutsu see main meetodis välja
+    //argumendi väärtused 4 ja 5
+    // loo uus meetod private static void checkArea(), kutsu checkArea() meetod välja getArea meetodist
+    //prindi konsooli "area is bigger than 20" kui pindala suurus ületab 20 ja "area is smaller than 20"
 
     public static void main(String[] args) {
 
-     Scanner sc = new Scanner(System.in);
+        getArea();
 
 
     }
 
-    private static void nameBigLetters(){
-
-
+    private static void getArea(){
+        int a = 4;
+        int b = 5;
+        int area =a*b;
+        System.out.println("Area size is: " + area);
+        checkArea(area);
     }
+    private static void checkArea(int area){
+        if (area > 20){
+            System.out.println("Area is bigger than 20");
+        } else if (area < 20) {
+            System.out.println("Area is smaller than 20");
+        }else{
+            System.out.println("Area is equal to 20");
+        }
+    }
+
+
 }
