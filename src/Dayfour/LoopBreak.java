69d9f224f881c5026941de886d2369a2a0e29b7b
Karin <karinhenn89@gmail.com>
2024-11-07 16:35:57 +0200

    Harjutused

---
+++ src/Dayfour/LoopBreak.java
@@ -0,0 +1,60 @@
+package Dayfour;
+
+public class LoopBreak {
+
+    //Break and Continue
+
+    public static void main(String[] args) {
+
+    firstEvenNumber();
+    oddNumbersPrint();
+    multiples();
+
+    }
+
+    private static void firstEvenNumber(){
+
+        //find first even number with loop, exit and print it on the screen
+        System.out.println("First even number, Exercise 1: ");
+
+        int[] numbers = {1,3,7,4,9,10};
+
+        for (int number : numbers){
+           if (number % 2==0){
+               System.out.println(number);
+               break;                                   //break - kirjutab selle paarisarvu ekraanile ja kohe peale seda lõpetab loop-imise
+           }
+        }
+
+    }
+    private static void oddNumbersPrint(){
+
+        //use a for loop to print all odd numbers between 1 and 20. if number is a multiple of 5, skip it using continue
+        System.out.println("Odd numbers, Exercise 2: ");
+
+        for (int i= 1; i<=20; i +=2){
+           if (i%5==0){
+               continue;
+           }
+           System.out.println(i);
+
+        }
+
+    }
+
+    private static void multiples(){
+        //print multiples  3 up to 30, skip multiples of 6
+        //use a for loop to print multiples up to 30.skip any multiples of 6 using continue
+        System.out.println("Multiples of three, Exercise 3: ");
+
+        int j = 3;
+        for (int i = 1; i <= 10; i++) {
+            int answer = i * j;
+            if (answer % 6 == 0) {
+                continue;
+            }
+            System.out.println(j + " x " + i + " = " + answer);
+        }
+
+    }
+}
