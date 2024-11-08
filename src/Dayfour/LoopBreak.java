daad30b45bec3dcd8d4c0e503d3b33ccaf72fcf7
Karin <karinhenn89@gmail.com>
2024-11-08 09:29:32 +0200

    Harjutused

--- src/Dayfour/LoopBreak.java
+++ src/Dayfour/LoopBreak.java
@@ -8,7 +8,7 @@ public class LoopBreak {
 
     firstEvenNumber();
     oddNumbersPrint();
-    multiples();
+    multiplesOf3SkipSix();
 
     }
 
@@ -19,7 +19,7 @@ public class LoopBreak {
 
         int[] numbers = {1,3,7,4,9,10};
 
-        for (int number : numbers){
+        for (int number : numbers){             //for each loop, mingi kogumi põhjal
            if (number % 2==0){
                System.out.println(number);
                break;                                   //break - kirjutab selle paarisarvu ekraanile ja kohe peale seda lõpetab loop-imise
@@ -32,28 +32,36 @@ public class LoopBreak {
         //use a for loop to print all odd numbers between 1 and 20. if number is a multiple of 5, skip it using continue
         System.out.println("Odd numbers, Exercise 2: ");
 
-        for (int i= 1; i<=20; i +=2){
+        for (int i= 1; i<=20; i +=2){           //only get odd numbers
            if (i%5==0){
-               continue;
+               continue;                        //skip numbers what are divided by 5
            }
-           System.out.println(i);
+           System.out.println(i);               //print out odd numbers, where there are no numbers in what divides by 5
 
         }
 
     }
 
-    private static void multiples(){
+    private static void multiplesOf3SkipSix() {
         //print multiples  3 up to 30, skip multiples of 6
         //use a for loop to print multiples up to 30.skip any multiples of 6 using continue
         System.out.println("Multiples of three, Exercise 3: ");
 
-        int j = 3;
+        /*int j = 3;                        //Ma ise tegin nii, ehk siis vastused said õiged, ag ama tegin korrutustabeli. oli ainult vastuseid vaja
         for (int i = 1; i <= 10; i++) {
             int answer = i * j;
             if (answer % 6 == 0) {
                 continue;
             }
             System.out.println(j + " x " + i + " = " + answer);
+        }
+        */
+
+        for (int i = 3; i <= 30; i += 3) {
+            if (i % 6 == 0) {
+                continue;           //skip the numbers that are multiples of 6
+            }
+            System.out.println(i);
         }
 
     }
