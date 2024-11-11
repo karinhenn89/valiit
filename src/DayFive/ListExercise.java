cebfb7b733e9d68714fda060771acc728475202e
Karin <karinhenn89@gmail.com>
2024-11-11 09:38:36 +0200

    Harjutused

--- src/DayFive/ListExercise.java
+++ src/DayFive/ListExercise.java
@@ -7,21 +7,30 @@ public class ListExercise {
 
     public static void main(String[] args) {
 
-        /*arrayListMistakeOne();*/
+        String text = "Hello";
+        System.out.println("\"Hello\" text lenght: " + text.length());
+
+        String text2 = "";
+        System.out.println("Text2 empty - true or false: " + text2.isEmpty());
+
+
+        arrayListMistakeOne();
         arrayListMistakeTwo();
     }
-//    private static void arrayListMistakeOne(){
-//        ArrayList<Integer> numbers = new ArrayList<>();
-//
-//        numbers.add(10);
-//        numbers.add(20);
-//        numbers.add(30);
-//
-//        for (int i = 0; i < numbers.size(); i++) {                                  //size loeb 1,2,3 - aga elemente on 0,1,2 - seega ei saa olla seal vahel et võrdub i
-//            System.out.println("Element at index " + i + ": " + numbers.get(i));
-//
-//        }
-//        }
+    private static void arrayListMistakeOne(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        System.out.println("List size: " + numbers.size());
+
+        for (int i = 0; i < numbers.size(); i++) {                                  //size loeb 1,2,3 - aga elemente on 0,1,2 - seega ei saa olla seal vahel et võrdub i
+            System.out.println("Element at index " + i + ": " + numbers.get(i));
+
+        }
+       }
     private static void arrayListMistakeTwo(){
 
         //remove elements from a list in a loop
@@ -32,9 +41,9 @@ public class ListExercise {
             for (int i = 1; i <= 10; i++) {
                 numbers.add(i);
             }
-        System.out.println("List, when numbers are added" + numbers);
+        /*System.out.println("List, when numbers are added" + numbers);*/
 
-           for (int i = numbers.size(); i >= 0; i--) {
+           for (int i = numbers.size()-1; i >= 0; i--) {        //-1 nr size, sest numbers size loetakse alates ühest(1-10), aga listis on numbrid alates 0-ist (0-9)
                 if (numbers.get(i) % 2 == 0) {
                     numbers.remove(i);
                 }
